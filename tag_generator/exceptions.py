9b1d0aaec16fd4135f9fc1341566bbfd5c1d69e4
Samuel Howard <samuelhowardstudent@gmail.com>
2021-07-19 15:28:53 -0400

    Adds tag generating code from strongswan

---
+++ tag_generator/exceptions.py
@@ -0,0 +1,31 @@
+# Code modified from https://github.com/strongswan/swidGenerator
+# -*- coding: utf-8 -*-
+from __future__ import print_function, division, absolute_import, unicode_literals
+
+
+class AutodetectionError(RuntimeError):
+    """
+    Raised when autodetecting the environment fails.
+    """
+    pass
+
+
+class EnvironmentNotInstalledError(RuntimeError):
+    """
+    Raised when a manually specified environment cannot be found on the system.
+    """
+    pass
+
+
+class RequirementsNotInstalledError(RuntimeError):
+    """
+    Raised when the requirements for a operation is not installed.
+    """
+    pass
+
+
+class CommandManagerError(RuntimeError):
+    """
+    Raised when CommandManager cannot run command.
+    """
+    pass
---
+++ tag_generator/package_info.py
@@ -0,0 +1,37 @@
+# Code modified from https://github.com/strongswan/swidGenerator
+# -*- coding: utf-8 -*-
+from __future__ import print_function, division, absolute_import, unicode_literals
+
+import os.path
+
+
+class FileInfo(object):
+    def __init__(self, path, actual_path=True):
+        self.name = (os.path.split(path)[1]).strip()
+        self.location = (os.path.split(path)[0]).strip()
+        self.mutable = False
+        self.full_pathname = '/'.join((self.location, self.name))
+
+        splitted_location = self.full_pathname.split('/')
+        self.full_pathname_splitted = splitted_location[1:]
+
+        if actual_path:
+            self.actual_full_pathname = self.full_pathname
+            self.size = str(os.path.getsize(self.full_pathname))
+        else:
+            self.actual_full_pathname = ""
+
+    def set_actual_path(self, file_path):
+        self.actual_full_pathname = file_path.encode('utf-8')
+        self.size = str(os.path.getsize(file_path))
+
+
+class PackageInfo(object):
+    def __init__(self, package='', version='', files=None, status=None, summary=None):
+        if files is None:
+            files = []
+        self.package = package
+        self.version = version
+        self.files = files
+        self.status = status
+        self.summary = summary
