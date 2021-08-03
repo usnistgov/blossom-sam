bc3de68d0f21deb3c05ae7e7f84d9c66461dadba
Lawrence Sebald <lsebald1@umbc.edu>
2021-08-02 21:08:39 -0400

    Add endpoint to fetch installer packages

--- webservice/service/sam.py
+++ webservice/service/sam.py
@@ -4,11 +4,32 @@ from flask import (
     jsonify,
     redirect,
     request,
-    response
+    response,
+    send_file
 )
 
+from pathlib import Path
+
 app = Flask(__name__)
+
+# TODO: Move this to a configuration file later.
+INSTALLER_BASE = '/opt/blossom-sam/installers'
+KEYFILES_BASE = '/opt/blossom-sam/keys'
+DB_FILE = '/opt/blossom-sam/sam.db'
 
 @app.route('/')
 def endpoint_root():
     return Response('BLoSS@M SAM Service Running...', mimetype='text/plain')
+
+# TODO: Implement authentication of clients connecting to below endpoints
+@app.route('/installer/<os:string>/<arch:string>/<app:string>/<ver:string>')
+def endpoint_inst(os, arch, app, ver):
+    # See if we have a directory for this particular request
+    p = Path(INSTALLER_BASE)
+    p2 = p / os / arch / app / ver / 'sam.pkg'
+    if not p2.exists():
+        return Response('', mimetype='text/plain'), 404
+
+    # Send the installer out
+    return send_file(p2, mimetype='application/zip', as_attachment=True,
+                     download_name=app + '-' + ver + '.pkg', etag=True)
